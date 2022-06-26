cf533e437ecd8e844beb596f0508910a41acb822
ShajidJeshan <shajidjeshan@gmail.com>
2022-06-27 02:20:17 +0600

    feat(api: DonorForm): Post Donor Info

--- app/core/api/urls.py
+++ app/core/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list-of-donors/', views.DonorList.as_view(), name='list-of-donor'),
+    path('donor/', views.DonorFormView.as_view(), name='donor'),
 ]
--- app/core/api/views.py
+++ app/core/api/views.py
@@ -70,11 +70,11 @@ class DonorList(APIView,CustomPageNumber):
         try:
             donor_list = Donor.objects.all().order_by('id')
             page = self.paginate_queryset(donor_list,request)
-            for i in page:
-                if 'NEG' in i.blood_group:
-                    i.blood_group = i.blood_group.replace('NEG','-')
-                elif 'POS' in i.blood_group:
-                    i.blood_group = i.blood_group.replace('POS','+')
+            # for i in page:
+            #     if 'NEG' in i.blood_group:
+            #         i.blood_group = i.blood_group.replace('NEG','-')
+            #     elif 'POS' in i.blood_group:
+            #         i.blood_group = i.blood_group.replace('POS','+')
             serializer = DonorSerializer(page, many=True)
             
             if(serializer.data):
@@ -91,3 +91,72 @@ class DonorList(APIView,CustomPageNumber):
             }
             return Response(res, status=status.HTTP_400_BAD_REQUEST)
  
+class DonorFormView(APIView):
+    """Donor Data Input API.
+
+    Args:
+        APIView ([class]): inherits default APIView of Django REST Framework(DRF)
+    """
+
+    permission_classes = (AllowAny, )
+
+    def post(self, request, format=None):
+        """Posting Donor data. Authentication does not require.
+
+        Args:
+            request ([POST]): http://127.0.0.1:8000/api/donor/
+
+        Returns:
+            [json]: Multiple dictionary object in a list.
+            {
+                "result": [
+                    {
+                        "id": 9,
+                        "firstname": "Imon",
+                        "lastname": "Hasan",
+                        "age": "26",
+                        "sex": "Male",
+                        "location": "Bogura",
+                        "blood_group": "B+"
+                    }
+                ]
+            }
+        """
+        try:
+            firstname = request.data['firstname']
+            lastname = request.data['lastname']
+            age = request.data['age']
+            sex = request.data['sex']
+            location = request.data['location']
+            blood_group = request.data['blood_group']
+            dic = {}
+            dic['firstname'] = firstname
+            dic['lastname'] = lastname
+            dic['age'] = age
+            dic['sex'] = sex
+            dic['location']=location
+            dic['blood_group'] =blood_group
+            
+            serializer= DonorSerializer(data=[dic],many=True)
+            if serializer.is_valid():
+                serializer.save()   
+            else:
+                res = {
+                    'result':'Invalid Data'
+                }
+                return Response(res, status=status.HTTP_400_BAD_REQUEST)
+            # if 'POS' in serializer.data[0]['blood_group']:
+            #     serializer.data[0]['blood_group']= serializer.data[0]['blood_group'].replace('POS','+')
+            # elif 'NEG' in serializer.data[0]['blood_group']:
+            #     serializer.data[0]['blood_group']= serializer.data[0]['blood_group'].replace('PNEG','-')
+            res={
+                'result':serializer.data
+                }
+
+            return Response(res, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            res = {
+                "error" : e.args[0]
+            }
+            return Response(res, status=status.HTTP_400_BAD_REQUEST)
