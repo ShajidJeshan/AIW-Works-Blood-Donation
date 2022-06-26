43690c3d52cc1bd130a15a087eb37a42f3aff044
ShajidJeshan <shajidjeshan@gmail.com>
2022-06-26 18:52:04 +0600

    feat(migration): Add DB model and migrate

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
 
-# Register your models here.
+@admin.register(Donor)
+class DonorAdmin(ImportExportModelAdmin):
+    list_display = ['firstname','lastname','age','sex','location','blood_group']
